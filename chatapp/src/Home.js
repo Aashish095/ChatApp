f3c338a769ec4978094a87dc57f135185e995163
aashish <aash.jain@stackfusion.io>
2023-02-26 13:59:36 +0530

    changes with chatapp

--- chatapp/src/Home.js
+++ chatapp/src/Home.js
@@ -1,45 +1,79 @@
-import React, {useEffect, useState} from "react";
-import {useLocation} from "react-router-dom";
-import userEvent from "@testing-library/user-event";
-
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
-    const {state} = useLocation();
-    const { name } = state;
-    let [chatSocket,setchatSocket]= useState("");
-    let [message,setmessage]= useState("");
+  const { state } = useLocation();
+  const [chatSocket, setchatSocket] = useState(null);
+  const [message, setmessage] = useState("");
+  const [textMessages, setTextMessages] = useState("");
 
-    useEffect(()=>{
-        chatSocket = new WebSocket(
-            'ws://'
-            + 'localhost'+':8000'
-            + '/ws/chat/'
-            + name
-            + '/'
-        );
-        console.log(chatSocket)
-    },[name]);
+  const name = state['info'];
 
-    const ChatRoomMessages = (event) => {
-        console.log(event.target)
+  useEffect(() => {
+    // connect the websocket with when name was changed
+    const ws = new WebSocket(
+      'ws://'
+      + 'localhost' + ':8000'
+      + '/ws/chat/'
+      + name + '/'
+    );
+
+    ws.onopen = () => {
+      console.log('WebSocket connection opened');
+    };
+
+    ws.onmessage = (event) => {
+      // # this will update the textmessage come from the channel
+      const data = JSON.parse(event.data);
+      const newMessage = data.message;
+      setTextMessages(prevMessages => {
+        return prevMessages + "\n" + newMessage;
+      });
+    };
+
+    ws.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    setchatSocket(ws);
+
+    return () => {
+      ws.close();
+    };
+  }, [name]);
+
+  const chatRoomMessages = (message) => {
+    if (chatSocket && chatSocket.readyState === WebSocket.OPEN) {
+      chatSocket.send(JSON.stringify({
+        'message': message
+      }));
+    } else {
+      console.log('WebSocket connection not open');
     }
+    setmessage("");
+  }
 
-    const inputMessage = (event) => {
-        setmessage(event.target.value)
-    }
+  const handleInputChange = (event) => {
+    setmessage(event.target.value);
+  }
 
-    return  (<>
-        <header>
-            <h1>welcome to chat Room</h1>
-             <textarea name="postContent" rows={4} cols={40} />
-            <input type="textarea"
-              name="textValue"
-              value={message}
-              onChange={inputMessage}
-            />
-             <button onClick={() => ChatRoomMessages()}>Submit</button>
-        </header>
-           </>
+  return (
+    <>
+      <header>
+        <h1>Welcome to Chat Room</h1>
+        {/*this will set the value come from the websocket and show in the textarea*/}
+        <textarea value={textMessages} rows={10} cols={50} readOnly />
+        {/*when we input something it show directly into the input and remove after click the submit*/}
+        <input
+          type="text"
+          name="textValue"
+          value={message}
+          onChange={handleInputChange}
+        />
+        {/*button call the chatroom message to send the massage through channel*/}
+        <button onClick={() => chatRoomMessages(message)}>Submit</button>
+      </header>
+    </>
   );
 }
 
